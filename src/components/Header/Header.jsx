9e3463c2f8cb913509e78568ce2468e384606b83
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-08-08 20:35:22 +0500

    start

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <p className='flex justify-center container'>
+            Header
+        </p>
+    </div>
+  )
+}
+
+export default Header
